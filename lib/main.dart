93730ddb0d85287ffa9fe097cd5f596a9d7a3282
Rifqi Setiawan <rifqis232@gmail.com>
2025-02-27 11:30:40 +0700

    menambahkan splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:get/get.dart';
 import 'firebase_options.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  print('WidgetsFlutterBinding initialized');
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  print('FlutterNativeSplash.preserve() called');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
 }
-  
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2), () {
+      FlutterNativeSplash.remove();
+    });
     return GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
